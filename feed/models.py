4758ad04c3f25eed957b4cae21c831b5dfae2c8f
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-10 14:43:33 +0000

    Add Post and Comment models

--- feed/models.py
+++ feed/models.py
@@ -9,3 +9,32 @@ class Topic(models.Model):
     content = models.TextField()
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
+
+
+class Post(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name="my_feed"
+    )
+    author = models.ForeignKey(
+        Topic,
+        on_delete=models.CASCADE,
+        related_name="my_feed"
+    )
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
