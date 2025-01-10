9ebc9106714316bfb1c2b2ccae14995157d9c6b0
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-10 13:47:57 +0000

    Add a Topic model

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
+class Topic(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
