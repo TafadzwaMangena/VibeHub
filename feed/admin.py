386d146ab5d5d3a765d51a315c85c5dedec02021
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-12 11:32:47 +0000

    Add summernote

--- feed/admin.py
+++ feed/admin.py
@@ -1,8 +1,18 @@
 from django.contrib import admin
 from .models import Topic, Post, Comment, Report
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Topic)
+class TopicAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'status')
+    search_fields = ['title']
+    list_filter = ('status',)
+    summernote_fields = ('content',)
+
 
 # Register your models here.
-admin.site.register(Topic)
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(Report)
--- vibehub/settings.py
+++ vibehub/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-tafadzwamangena-vibehub-x578m0a2g2b.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_summernote',
     'feed',
 ]
 
