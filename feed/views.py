7a184cb2ccbbd6eca9de3b57d3c74cff0d68a8be
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-15 12:04:57 +0000

    Update post views on feed

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,31 @@
 from django.shortcuts import render
 from django.views import generic
-from .models import Topic
+from django.utils import timezone
+from .models import Topic, Post
 
 # Create your views here.
 class TopicList(generic.ListView):
     queryset = Topic.objects.all()
     template_name = "feed/index.html"
     paginate_by = 10
+
+posts = [
+    {
+        'author': 'Tafadzwa testing',
+        'title': 'Post 1',
+        'content': 'content test',
+        'date_posted': '12 January, 2025'
+    },
+    {
+        'author': 'Tafadzwa testing2',
+        'title': 'Post 2',
+        'content': 'content test2',
+        'date_posted': '12 January, 2025'
+    }
+]
+
+def my_feed(request):
+    context = {
+        'posts': Post.objects.all
+    }
+    return render(request, 'feed/index.html', context)
