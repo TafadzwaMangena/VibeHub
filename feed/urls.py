48951ba7f787d88a361fd8db1949858f6e1e89db
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-15 13:04:03 +0000

    Update feed views, url and template

--- feed/urls.py
+++ feed/urls.py
@@ -2,5 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.TopicList.as_view(), name='feed'),
+    # path('', views.TopicList.as_view(), name='feed'),
+    path('', views.my_feed, name='feed'),
 ]
--- feed/views.py
+++ feed/views.py
@@ -4,28 +4,24 @@ from django.utils import timezone
 from .models import Topic, Post
 
 # Create your views here.
-class TopicList(generic.ListView):
-    queryset = Topic.objects.all()
-    template_name = "feed/index.html"
-    paginate_by = 10
+# class TopicList(generic.ListView):
+#     queryset = Topic.objects.all()
+#     template_name = "feed/index.html"
+#     paginate_by = 10
 
-posts = [
-    {
-        'author': 'Tafadzwa testing',
-        'title': 'Post 1',
-        'content': 'content test',
-        'date_posted': '12 January, 2025'
-    },
-    {
-        'author': 'Tafadzwa testing2',
-        'title': 'Post 2',
-        'content': 'content test2',
-        'date_posted': '12 January, 2025'
-    }
-]
+#     def my_feed(request):
+#     context = {
+#         'posts': Post.objects.all
+#     }
+#     return render(request, 'feed/index.html', context)
 
 def my_feed(request):
+    topics = Topic.objects.all()
+    posts = Post.objects.all()
+
     context = {
-        'posts': Post.objects.all
+        'topics': topics,
+        'posts': posts,
     }
+
     return render(request, 'feed/index.html', context)
