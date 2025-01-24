cda6656c687c25010c8ac7d2b3541847eedd274c
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-24 22:46:03 +0000

    Add js file to staticfiles

--- vibehub/settings.py
+++ vibehub/settings.py
@@ -27,7 +27,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-tafadzwamangena-vibehub-x578m0a2g2b.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
