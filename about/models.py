df162c33c535b1bbc1f00adf3b030087d88b91a3
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-28 00:27:12 +0000

    Update about views and about.html file

--- about/models.py
+++ about/models.py
@@ -1,7 +1,5 @@
 from django.db import models
 
-from django.db import models
-
 
 class About(models.Model):
     title = models.CharField(max_length=200)
--- about/views.py
+++ about/views.py
@@ -6,7 +6,7 @@ def about_vibehub(request):
     """
     Renders the About page
     """
-    about = About.objects.all()
+    about = About.objects.all().first()
 
     return render(
         request,
