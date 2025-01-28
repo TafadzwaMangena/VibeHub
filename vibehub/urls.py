8f113392937421ec19a0088d2c410527515ef80b
Tafadzwa Mangena <mangenatafadzwamgcini@gmail.com>
2025-01-28 00:00:14 +0000

    Update about views, urls, admin and create about.html. Add corresponding code to project urls and base.html

--- vibehub/urls.py
+++ vibehub/urls.py
@@ -20,9 +20,9 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
+    path("about/", include("about.urls"), name="about-urls"),
     path('admin/', admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    #path('register/', user_views.register, name='register'),
     path('summernote/', include('django_summernote.urls')),  
     path("", include("feed.urls"), name="feed-urls"),  
 ]
