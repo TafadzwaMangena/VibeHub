dbbf1fb5715bf36b704f2d7876752445c80df31c
TafadzwaMangena <mangenatafadzwamgcini@gmail.com>
2025-01-31 22:13:49 +0100

    Add doc strings to js functions

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,29 @@
+/**
+ * Displaying django message.
+ */
 document.addEventListener("DOMContentLoaded", function () {
     const djangoMessage = document.getElementById("django-message");
 
     if (djangoMessage) {
-        // Show the message
+        // Make the message visible
         djangoMessage.style.display = "block";
-        
-        // To fade out the message after 10 seconds
+
+        // Automatically fade out the message after 10 seconds
         setTimeout(() => {
-            djangoMessage.style.opacity = "0";
-            setTimeout(() => djangoMessage.style.display = "none", 1000);
+            djangoMessage.style.opacity = "0"; // Gradually fade out
+            setTimeout(() => djangoMessage.style.display = "none", 1000); // Hide it completely after fading
         }, 10000);
     }
 });
 
-// Add tooltip functionality to elements with the "tooltip" data attribute
+/**
+ * Enable Bootstrap tooltips for disabled report button.
+ */
 document.addEventListener("DOMContentLoaded", function () {
+    // Select all elements with the tooltip attribute
     var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+
+    // Initialize Bootstrap tooltips for each element found
     var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
         return new bootstrap.Tooltip(tooltipTriggerEl);
     });
