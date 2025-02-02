38dab292977fa06478f9c3b909f0ec215bf960ae
TafadzwaMangena <mangenatafadzwamgcini@gmail.com>
2025-02-02 06:03:39 +0100

    Prep for deployment

--- feed/views.py
+++ feed/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import JsonResponse
+from django.http import JsonResponse # noqa
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator
-from django.utils import timezone
+from django.utils import timezone # noqa
 from .models import Topic, Post, Report
 from .forms import PostForm
 
